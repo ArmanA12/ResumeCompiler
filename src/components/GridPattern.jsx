fbf278c53da47989e56b3013d3ac08da330474d5
Arman Shekh <97970564+ArmanA12@users.noreply.github.com>
2025-01-30 12:23:52 +0530

    Update GridPattern.jsx

--- src/components/GridPattern.jsx
+++ src/components/GridPattern.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 
 
 const GridComponent = () => {
-  const columns = 51;
+  const columns = 47;
   const rows = 11;
   const totalCells = columns * rows;
   const [animatedCells, setAnimatedCells] = useState([]);
 
-  // Function to randomly select cells for animation
+
   const randomizeAnimation = () => {
-    const numberOfAnimatedCells = 10; // Number of cells to animate
+    const numberOfAnimatedCells = 10; 
     const newAnimatedCells = [];
 
     while (newAnimatedCells.length < numberOfAnimatedCells) {
@@ -21,8 +21,6 @@ const GridComponent = () => {
 
     setAnimatedCells(newAnimatedCells);
   };
-
-  // Randomize animation every 3 seconds
   useEffect(() => {
     const interval = setInterval(randomizeAnimation, 3000);
     return () => clearInterval(interval);
